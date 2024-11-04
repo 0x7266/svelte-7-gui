679ec03313368a9a1ca95dcb503fe24b24b1024e
rafael <rrrr0ffff@gmail.com>
2024-11-03 23:47:02 -0300

    center main components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	let { children } = $props();
 </script>
 
-<main class="h-dvh flex gap-2 bg-zinc-900">
+<div class="h-dvh flex bg-zinc-900">
 <aside class="h-full bg-zinc-800 border-r-2 border-r-zinc-700 flex flex-col gap-2 p-6 font-semibold font-mono text-xl text-zinc-300">
   <a class="hover:text-zinc-50" aria-label="counter" href="/counter">Counter</a>
   <a class="hover:text-zinc-50" aria-label="temperature converter" href="/temperatureconverter">Temperature converter</a>
@@ -13,5 +13,7 @@
   <a class="hover:text-zinc-50" aria-label="circle drawer" href="/circledrawer">Circle drawer</a>
   <a class="hover:text-zinc-50" aria-label="cells" href="/cells">Cells</a>
 </aside>
+<main class="w-full flex items-center justify-center">
 {@render children()}
 </main>
+</div>
